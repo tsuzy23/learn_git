b1c67b13b3a58f0f127a529d0146403d02de4776
都築梨花 <tsudzukirika@tsudzukirikanoMacBook-puro.local>
2022-03-09 15:52:42 +0900

    カウンター機能の追加

---
+++ svelte-rinka/src/routes/test1.svelte
@@ -0,0 +1,27 @@
+<!-- JavaScript -->
+<script lang="ts">
+    // 変数宣言　数値型　最初は０を入れる
+    let count: number = 0;  // 数値
+    let name: string = "rinka"; // 文字列
+
+    // さっきの変数に１プラスする変数を入れる
+    const clickHandler = () => {
+        count++;    // インクリメント
+        console.log(count);
+    }
+</script>
+
+<!-- html -->
+<body>
+    <h1>
+        {count}
+    </h1>
+    <button on:click={clickHandler}>
+        <p>＋</p>
+    </button>
+</body>
+
+<!-- css -->
+<style>
+
+</style>
